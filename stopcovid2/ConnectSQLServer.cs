860d9e484d7ec08844309254199f3adffc8784d4
Deyae zouggari <66757470+deyae-zouggari@users.noreply.github.com>
2020-12-24 02:33:20 +0100

    Update ConnectSQLServer.cs

--- stopcovid2/ConnectSQLServer.cs
+++ stopcovid2/ConnectSQLServer.cs
@@ -4,6 +4,20 @@ using System.Text;
 
 namespace stopcovid2
 {
-  
+  class ConnectSQLServer
+    {
+        public static SqlConnection GetDBConnection(string datasource, string database)
+        {
+            //
+            // Data Source=TRAN-VMWARE\SQLEXPRESS;Initial Catalog=simplehr;Persist Security Info=True;User ID=sa;Password=12345
+            //
+            string connString = @"Data Source=DESKTOP-D7HHOO9;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            return conn;
+        }
+
+    }
     
 }
