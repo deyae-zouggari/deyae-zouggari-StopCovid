8da32ac2c822e40245ebef81d1ff3f6fee11ebf0
Deyae zouggari <66757470+deyae-zouggari@users.noreply.github.com>
2020-12-24 01:48:38 +0100

    Update citoyen.cs

--- stopcovid2/citoyen.cs
+++ stopcovid2/citoyen.cs
@@ -7,31 +7,39 @@ namespace stopcovid2
 {
     class citoyen
     {
-        private static void OpenSqlConnection()
+         static string chaine = @"Data Source=DESKTOP-D7HHOO9;Integrated Security=True";
+        //"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\App_Data\VotreBD.mdf;Integrated Security=True;User Instance=True"
+        //"Server=.\SQLEXPRESS; DataBase=VotreBD;USER ID=sa; PASSWORD="
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        ConnectSQLServer A = new ConnectSQLServer();
+         public void InformationDuPersonne()
         {
-            string connectionString = GetConnectionString();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                Console.WriteLine("ServerVersion: {0}", connection.ServerVersion);
-                Console.WriteLine("State: {0}", connection.State);
-            }
-        }
-
-        static private string GetConnectionString()
-        {
-            // To avoid storing the connection string in your code,
-            // you can retrieve it from a configuration file, using the
-            // System.Configuration.ConfigurationManager.ConnectionStrings property
-            return " Data Source = DESKTOP - D7HHOO9;Initial Catalog = StopCovid;"
-                + "Integrated Security = True;";
-        }
-        public void informationDuPersonne(int id,string nom,string prenom,int age,string cin,bool pcr)
-        {
+            cnx.Open();
+            cmd.Connection = cnx;
             Console.WriteLine("entrer votre nom:");
             string Nom = Console.ReadLine();
-
-
+            Console.WriteLine("entrer votre prenom:");
+            string Prenom = Console.ReadLine();
+            Console.WriteLine("entrer votre age:");
+            string Age = Console.ReadLine();
+            Console.WriteLine("entrer votre cin:");
+            string CIN = Console.ReadLine();
+            Console.WriteLine("es ce que vous avez fait le test:");
+            string Pcr = Console.ReadLine();
+            cmd.CommandText = "insert into personne(nom)values (@nom);";
+            cmd.Parameters.AddWithValue("@nom", Nom);
+            cmd.CommandText = "insert into personne(prenom)values (@prenom);";
+            cmd.Parameters.AddWithValue("@prenom", Prenom);
+            cmd.CommandText = "insert into personne(age)values (@age);";
+            cmd.Parameters.AddWithValue("@age", Age);
+            cmd.CommandText = "insert into personne(cin)values (@cin);";
+            cmd.Parameters.AddWithValue("@cin", CIN);
+            cmd.CommandText = "insert into personne(pcr)values (@pcr);";
+            cmd.Parameters.AddWithValue("@pcr", Pcr);
+            cmd.ExecuteNonQuery();
+            cnx.Close();
         }
     }
 }
