bddeb7b7a25d66feca0e6a1140678df87a2e9f30
Deyae zouggari <66757470+deyae-zouggari@users.noreply.github.com>
2020-12-24 01:52:08 +0100

    Update ConnectSQLServer.cs

--- stopcovid2/ConnectSQLServer.cs
+++ stopcovid2/ConnectSQLServer.cs
@@ -4,12 +4,6 @@ using System.Text;
 
 namespace stopcovid2
 {
-    static void Main(string[] args)
-        {
-            citoyen a = new citoyen();
-            a.InformationDuPersonne();
-           
-            
-        }
+  
     
 }
