968586bdf8913f0a2c3f371a734a0be61f1a7831
Deyae zouggari <66757470+deyae-zouggari@users.noreply.github.com>
2020-12-28 04:51:32 +0100

    Add files via upload
    namespace stopcovid2
    {
    
        [Serializable]
         
        class citoyen
            {
            public string Nom;
            public string Prenom;
            public string Age;
            public string CIN;
            public string contact;
            public string malad;
            public string vacc;
            public citoyen()
            {
    
            }
    
    
            public citoyen(string Nom,string Prenom,string Age,string CIN,string contact,string malad,string vacc)
            {
                this.Nom=Nom;
                this.Prenom = Prenom;
                this.Age = Age;
                this.CIN = CIN;
                this.contact = contact;
                this.malad = malad;
                this.vacc = vacc;
    
            }
    
            public void Information()
                 {
               /* cnx.Open();
                cmd.Connection = cnx;*/
                Console.WriteLine("entrer votre nom:");
                 this.Nom = Console.ReadLine();
                Console.WriteLine("entrer votre prenom:");
                 this.Prenom = Console.ReadLine();
                Console.WriteLine("entrer votre age:");
                this.Age = Console.ReadLine();
                Console.WriteLine("entrer votre CIN:");
               this.CIN = Console.ReadLine();
    
            
            public bool Sumptomes()
            {
                Console.WriteLine("es ce que vous avez des sumptomes de covid: ");
                string S = Console.ReadLine();
                if (S == "oui")
                {
                    return true;
                }
                else return false;
            }
            public bool AfaireLeTest()
            {
                Console.WriteLine("es ce que vous avez fait le test: ");
                String T = Console.ReadLine();
                if (T =="oui")
                {
                    return true;
                }
                else return false;
    
            }
            public bool EtatTest()
            {
                Console.WriteLine("es ce que le test est positive: ");
                string etat = Console.ReadLine();
                if (etat == "oui")
                {
                    this.malad = "malad";
                    return true;
                }
                else return false;
            }
    
            public bool Contacter()
            {
                Console.WriteLine("es ce que vous avez contacter quelqu'un malade :");
                string cont = Console.ReadLine();
                if (cont == "oui")
                {
                    this.contact = "contacteur";
                    return true;
    
                }
                else return false;
            }
              
            
            public void TestPositive()
            {
                Console.WriteLine("Votre Code est : Rouge");
                Console.WriteLine("Vous de vez confiner d'une duree de 15 jours:");
            }
           
            public bool Vaccination()
            {
                Console.WriteLine("es ce que vous etes vaccine ? ");
                string vac = Console.ReadLine();
                if (vac == "oui")
                {
                   this.vacc = "vaccine"; 
                    return true;
                }
                else
                {
                    return false;
                }
    
            }
            
           
            public override string ToString()
            {
                
                return ("Information sur la personne : nom :" + Nom + " prenom :" + Prenom + " age :" + Age + " Etat :" + malad + "contacter :" + contact + "vaccin :" + vacc);
            }
    
    
    
            // end public bool Load()
        }

---
+++ ConnectSQLServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Linq;
+
+
+namespace stopcovid2
+{
+    class ConnectSQLServer
+    {
+        public static SqlConnection GetDBConnection(string datasource, string database)
+        {
+            //
+            // Data Source=TRAN-VMWARE\SQLEXPRESS;Initial Catalog=simplehr;Persist Security Info=True;User ID=sa;Password=12345
+            //
+            string connString = @"Data Source=DESKTOP-D7HHOO9;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(connString);
+            
+
+            return conn;
+        }
+
+    }
+}
+
