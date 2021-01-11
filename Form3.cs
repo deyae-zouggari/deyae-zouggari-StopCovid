f9f6d2ab4c4c7a52f8fd26bb470b86b8e58df690
Deyae zouggari <66757470+deyae-zouggari@users.noreply.github.com>
2021-01-11 22:00:45 +0100

    Update Form3.cs

--- Form3.cs
+++ Form3.cs
@@ -10,15 +10,28 @@ using System.Windows.Forms;
 
 namespace stopecovideinterface
 {
-    public partial class Form3 : Form
+   public partial class Form3 : Form
     {
-    static string chaine = @"Data Source=DESKTOP-D7HHOO9;Initial Catalog=StopCovid;Integrated Security=True";
+        public static int day;
+        private DateTime endsAt;
+
+  
+
+        // These integer variables store the numbers 
+        // for the addition problem. 
+
+
+        // This integer variable keeps track of the 
+        // remaining time.
+       
+
+        static string chaine = @"Data Source=DESKTOP-D7HHOO9;Initial Catalog=StopCovid;Integrated Security=True";
         //"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\App_Data\VotreBD.mdf;Integrated Security=True;User Instance=True"
         //"Server=.\SQLEXPRESS; DataBase=VotreBD;USER ID=sa; PASSWORD="
         static SqlConnection cnx = new SqlConnection(chaine);
         static SqlCommand cmd = new SqlCommand();
         static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-      
+
         public Form3()
         {
             InitializeComponent();
@@ -31,7 +44,9 @@ namespace stopecovideinterface
             f2.ShowDialog();
             this.Close();
         }
-         private void textBox1_TextChanged(object sender, EventArgs e)
+
+
+        private void textBox1_TextChanged(object sender,EventArgs e)
         {
 
         }
@@ -40,9 +55,11 @@ namespace stopecovideinterface
         {
             cnx.Open();
             cmd.Connection = cnx;
-            cmd.CommandText = "select Nom,Prenom,Age,CIN,Malad,Vaccine,pictur from Personne where CIN='"+textBox1.Text+"'";
+            cmd.CommandText = "select Nom,Prenom,Age,CIN,Malad,Vaccine,dayc from Personne where CIN='" + textBox1.Text + "'";
             SqlDataReader reader = cmd.ExecuteReader();
-            
+            //byte[] pictureIdByte;
+           // Image pictureIdImg = null;
+
             reader.Read();
             textBox2.Text = reader.GetString(0);
             textBox3.Text = reader.GetString(1);
@@ -50,15 +67,46 @@ namespace stopecovideinterface
             textBox5.Text = reader.GetString(3);
             textBox6.Text = reader.GetString(4);
             textBox7.Text = reader.GetString(5);
-            DataSet ds = new DataSet();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(ds);
-           
-                MemoryStream ms = new MemoryStream((byte[])ds.Tables[0].Rows[0]["pictur"]);
+            day = reader.GetInt32(6);
+            textBox8.Text = day.ToString();
 
-                pictureBox1.Image = new Bitmap(ms);
-            
+            //pictureIdByte = (byte[])(reader["pictur"]);
+
+            //pictureBox1.Image = pictureIdImg;
+
+
             cnx.Close();
+            //cnx.Open();
+            //cmd.Connection = cnx;
+            //cmd.CommandText = "select pictur from Personne where CIN='" + textBox1.Text + "'";
+            //DataSet dataSet = new DataSet();
+            //adapter.Fill(dataSet);
+            //byte[] image = File.ReadAllBytes("pictur");
+            //if (dataSet.Tables[0].Rows.Count == 1)
+            //{
+            //    MemoryStream mem = new MemoryStream("pictur");
+            //    pictureBox1.Image = Image.FromStream(mem);
+            //}
+            //cnx.Close();
+            
+
+        }
+        // returns today's date
+      
+
+
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            int secondsRemaining = (int)(endsAt - DateTime.Now).TotalSeconds;
+            TimeSpan time = TimeSpan.FromSeconds(secondsRemaining);
+            label1.Text = time.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            endsAt = DateTime.Now.AddDays(day);  
         }
     }
 }
